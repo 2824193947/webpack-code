9c0ecb02decaefd16785f3a24abf3d6566538973
zky2824193947 <2824193947@qq.com>
2022-10-30 15:04:43 +0800

    添加code2文件, 深入理解webpack配置

---
+++ code2/03_split_chunk/src/dist/runtime/main.21e7811a5ab370b6ee8b.js
@@ -0,0 +1,17 @@
+(self["webpackChunk_03_split_chunk"] = self["webpackChunk_03_split_chunk"] || []).push([[179],{
+
+/***/ 893:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+__webpack_require__.e(/* import() | sum */ 644).then(__webpack_require__.bind(__webpack_require__, 548)).then(m => {
+    console.log(m.default(3, 4))
+})
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__(893));
+/******/ }
+]);
